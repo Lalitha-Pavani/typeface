11d453a7191cfdd483c4d14df4e1a67aaa010a50
Lalitha <104770611+Lalitha-Pavani@users.noreply.github.com>
2022-11-21 16:10:48 +0530

    Create 1.py
    Given an integer as input. Write a function to convert that to base 3 number
    
    Input: 10 
    Output: 101

---
+++ 1.py
@@ -0,0 +1,12 @@
+def tobase3(n):
+    res=''
+    while n!=0:
+        rem = n%3 
+        n = n//3 
+        res+=str(rem)
+    return int(res)
+ 
+    
+    
+num = int(input())
+print(tobase3(num))
